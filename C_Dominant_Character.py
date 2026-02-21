8b7328acfb1c60c66bb99d8c1a3c98618922ad16
haymi_g <haymig75@gmail.com>
2026-02-21 16:41:27 +0300

    C_Dominant_Character codeforce solution

--- C_Dominant_Character.py
+++ C_Dominant_Character.py
@@ -2,20 +2,20 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     s = input()
-
     ans = -1
 
-   
+  
     for i in range(n - 1):
-        if s[i] == 'a' and s[i + 1] == 'a':
+        sub = s[i:i+2]
+        if sub.count('a') > sub.count('b') and sub.count('a') > sub.count('c'):
             ans = 2
             break
 
    
     if ans == -1:
         for i in range(n - 2):
-            sub = s[i:i + 3]
-            if sub.count('a') == 2:
+            sub = s[i:i+3]
+            if sub.count('a') > sub.count('b') and sub.count('a') > sub.count('c'):
                 ans = 3
                 break
 
