49a954f2105015c3ed844c7deb9ba0e0e414da15
haymanot Getachew <haymig75@gmail.com>
2026-03-11 18:02:43 +0300

    Time: 83 ms (91.96%), Space: 28.1 MB (88.05%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n  = len(temperatures)
+        stack = []
+        result = [0] * n
+        for i,temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                prev_idx = stack.pop()
+                result[prev_idx] = i - prev_idx
+                
+            stack.append(i)
+        return result
+        
