a6e1c4566ecefc9db62cbc1973597680d0e5125f
haymanot Getachew <haymig75@gmail.com>
2026-03-13 16:39:02 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sorted_s = sorted(s)
-        sorted_t = sorted(t)
-        return sorted_s == sorted_t
+        if len(s) != len(t):
+            return False
+        my_dict = defaultdict(int)
+
+        for i in s:
+            my_dict[i] += 1
+
+        for j in t :
+            if j not in my_dict or my_dict[j] == 0 :
+                return False
+            my_dict[j] -= 1
+        return True
+        
+            
