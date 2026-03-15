8b9b512d0598af719feac9e03c41b0a95f78e21b
haymanot Getachew <haymig75@gmail.com>
2026-03-15 08:01:34 +0300

    Time: 35 ms (17.58%), Space: 18 MB (23.65%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,32 +1,26 @@
 class Solution(object):
     def groupAnagrams(self, strs):
+        
         groups = defaultdict(list)
-        
-        for word in strs:
-            
-           words = tuple(sorted(word))
-           groups[words].append(word)
-        return  list(groups.values())
-        
+
+       
+        for s in strs:
+            count = [0] * 26
+            for char in s :
+                count[ord(char) - ord('a')] += 1
+            key = tuple(count)
+            groups[key].append(s)
+        return groups.values()
 
             
-           
 
-        #  for word in words:
-        #         common_word = counter(word) 
-            # for group in groups:
-            #     if Counter(word) == Counter(group[0]):
-            #         group.append(word)
-            #         break
-            # else:
-            #     groups.append([word])
-        # return groups
+        
+    
 
 
 
 
 
 
-
-        # return List(groups.values()) 
+      
             
